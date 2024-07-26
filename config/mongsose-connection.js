1a2aaf13331fde762850b75e468fa2c5f11810d6
gajendraverma <gajendraverma353@gmail.com>
2024-07-27 01:01:43 +0530

    app

--- config/mongsose-connection.js
+++ config/mongsose-connection.js
@@ -1,9 +1,10 @@
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/khatabookcompleted', {
+mongoose.connect('mongodb://127.0.0.1:27017/khatabookcompleted', {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 10000 
 }).then(() => {
   console.log('Database connection successful');
 }).catch((err) => {

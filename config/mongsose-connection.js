78c5aa958d0480f99f67a8fef5cdee3ac9ac53cb
gajendraverma <gajendraverma353@gmail.com>
2024-07-27 16:32:24 +0530

    name

--- config/mongsose-connection.js
+++ config/mongsose-connection.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://gajendraverma353:5Yk0wRwJm8jNSFAY@cluster0.bt6pfsh.mongodb.net/Khatabook', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 10000 
+  serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
+  socketTimeoutMS: 30000,  
 }).then(() => {
   console.log('Database connection successful');
 }).catch((err) => {
